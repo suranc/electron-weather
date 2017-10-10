9bc42f5f0e6bd5174fb282582bef9b238f3c7213
Chris Suran <suran.c@gmail.com>
2017-10-10 23:17:48 +0100

    adding steps 1-5

--- main.js
+++ main.js
@@ -9,79 +9,87 @@ let window = undefined
 process.env.GOOGLE_API_KEY = "AIzaSyAjQz5Scgjt8A-n_PKQWjNiYg-ht2gUJts"
 
 
-app.on('ready', () => {
-  createTray()
-  createWindow()
-})
+  var menubar = require('menubar')
+  var mb = menubar()
 
-// Quit the app when the window is closed
-app.on('window-all-closed', () => {
-  app.quit()
-})
-
-const createTray = () => {
-  tray = new Tray(path.join(assetsDirectory, 'sunTemplate.png'))
-  tray.on('right-click', toggleWindow)
-  tray.on('double-click', toggleWindow)
-  tray.on('click', function (event) {
-    toggleWindow()
-
-    // Show devtools when command clicked
-    if (window.isVisible() && process.defaultApp && event.metaKey) {
-      window.openDevTools({mode: 'detach'})
-    }
+  mb.on('ready', function ready () {
+    console.log('app is ready')
+    // your app code here
   })
-}
 
-const getWindowPosition = () => {
-  const windowBounds = window.getBounds()
-  const trayBounds = tray.getBounds()
+// app.on('ready', () => {
+  // createTray()
+  // createWindow()
+// })
 
-  // Center window horizontally below the tray icon
-  const x = Math.round(trayBounds.x + (trayBounds.width / 2) - (windowBounds.width / 2))
+// // Quit the app when the window is closed
+// app.on('window-all-closed', () => {
+  // app.quit()
+// })
 
-  // Position window 4 pixels vertically below the tray icon
-  const y = Math.round(trayBounds.y + trayBounds.height + 4)
+// const createTray = () => {
+  // tray = new Tray(path.join(assetsDirectory, 'sunTemplate.png'))
+  // tray.on('right-click', toggleWindow)
+  // tray.on('double-click', toggleWindow)
+  // tray.on('click', function (event) {
+    // toggleWindow()
 
-  return {x: x, y: y}
-}
+    // // Show devtools when command clicked
+    // if (window.isVisible() && process.defaultApp && event.metaKey) {
+      // window.openDevTools({mode: 'detach'})
+    // }
+  // })
+// }
 
-const createWindow = () => {
-  window = new BrowserWindow({
-    
-    
-    show: false,
-    frame: false,
-    fullscreenable: false,
-    resizable: false,
-    transparent: true,
-    webPreferences: {
-      // Prevents renderer process code from not running when window is
-      // hidden
-      backgroundThrottling: false
-    }
-  })
-  window.loadURL(`file://${path.join(__dirname, 'index.html')}`)
+// const getWindowPosition = () => {
+  // const windowBounds = window.getBounds()
+  // const trayBounds = tray.getBounds()
 
-  // Hide the window when it loses focus
-  window.on('blur', () => {
-    if (!window.webContents.isDevToolsOpened()) {
-      window.hide()
-    }
-  })
-}
+  // // Center window horizontally below the tray icon
+  // const x = Math.round(trayBounds.x + (trayBounds.width / 2) - (windowBounds.width / 2))
 
-const toggleWindow = () => {
-  if (window.isVisible()) {
-    window.hide()
-  } else {
-    showWindow()
-  }
-}
+  // // Position window 4 pixels vertically below the tray icon
+  // const y = Math.round(trayBounds.y + trayBounds.height + 4)
 
-const showWindow = () => {
-  const position = getWindowPosition()
-  window.setPosition(position.x, position.y, false)
-  window.show()
-  window.focus()
-}
+  // return {x: x, y: y}
+// }
+
+// const createWindow = () => {
+  // window = new BrowserWindow({
+    // width: 300,
+    // height: 450,
+    // show: true,
+    // frame: false,
+    // fullscreenable: false,
+    // resizable: true,
+    // transparent: true,
+    // webPreferences: {
+      // // Prevents renderer process code from not running when window is
+      // // hidden
+      // backgroundThrottling: false
+    // }
+  // })
+  // window.loadURL(`file://${path.join(__dirname, 'index.html')}`)
+
+  // // Hide the window when it loses focus
+  // window.on('blur', () => {
+    // if (!window.webContents.isDevToolsOpened()) {
+      // window.hide()
+    // }
+  // })
+// }
+
+// const toggleWindow = () => {
+  // if (window.isVisible()) {
+    // window.hide()
+  // } else {
+    // showWindow()
+  // }
+// }
+
+// const showWindow = () => {
+  // const position = getWindowPosition()
+  // window.setPosition(position.x, position.y, false)
+  // window.show()
+  // window.focus()
+// }
